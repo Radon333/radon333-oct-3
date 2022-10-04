baebe583cf76502a6e4f8a0e5ccdcb9a3a6ae4f0
Ranveer <ranveer2001s@gmail.com>
2022-10-05 00:02:54 +0530

    Added landing page with animations and transitions

--- src/Components/PostModal.js
+++ src/Components/PostModal.js
@@ -18,14 +18,18 @@ import ImageRoundedIcon from "@mui/icons-material/ImageRounded";
 import EmojiEmotionsRoundedIcon from "@mui/icons-material/EmojiEmotionsRounded";
 import SendIcon from "@mui/icons-material/Send";
 import CancelIcon from '@mui/icons-material/Cancel';
+import { motion } from "framer-motion";
+import Fade from '@mui/material/Fade';
+import Backdrop from '@mui/material/Backdrop';
+
 
 const options = [
   "",
   <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-    <PublicIcon fontSize="small" /> Public: Visible to everyone 
+    <PublicIcon fontSize="small" /> Public: Visible to everyone
   </div>,
   <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-    <LockIcon fontSize="small" /> Private: Visible to your followers 
+    <LockIcon fontSize="small" /> Private: Visible to your followers
   </div>,
 ];
 
@@ -95,147 +99,198 @@ function PostModal() {
     image: { maxWidth: "50%", maxHeight: "50%" },
   };
 
+
+
   return (
     <div>
-      <Button onClick={handleOpen}>Open modal</Button>
+
+
+      <div style={{ backgroundColor: "#161b22", height: "100vh" }}>
+        <br />
+
+        <div style={{ paddingTop: "20vh", textAlign: "center" }}>
+          <motion.div initial={{ y: -400 }} animate={{ y: 0 }} transition={{ delay: 0.5, duration: 0.5 }}>
+            <h1
+              style={{
+                color: "#fc4273",
+                fontSize: "5rem",
+              }}
+            >
+              Bleep
+            </h1>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1, duration: 2 }}
+          >
+            <p style={{ textAlign: "center", color: "white", fontSize: "x-large" }}>
+              A soical media platform
+            </p>
+            <Button
+              onClick={handleOpen}
+              style={{ textDecoration: "none" }}
+              size="large"
+              variant="contained"
+              color="primary"
+            >
+              Post Something
+            </Button>
+          </motion.div>
+
+        </div>
+      </div>
+
+
+
+
+
+
+
       <Modal
         open={open}
         onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+        aria-labelledby="Create a Post"
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
       >
-        <Box sx={style}>
-          <div
-            className="topBar"
-            style={{ display: "flex", justifyContent: "space-between" }}
-          >
-            <Typography id="modal-modal-title" variant="h5" component="h2">
-              Create a new post
-            </Typography>
-            <CloseIcon style={{cursor:"pointer"}}onClick={handleClose} />
-          </div>
-          <hr />
+        <Fade in={open}>
+          <Box sx={style}>
+            <div
+              className="topBar"
+              style={{ display: "flex", justifyContent: "space-between" }}
+            >
+              <Typography id="modal-modal-title" variant="h5" component="h2">
+                Create a new post
+              </Typography>
+              <CloseIcon style={{ cursor: "pointer" }} onClick={handleClose} />
+            </div>
+            <hr />
 
-          <div
-            className="profile"
-            style={{ paddingTop:"1vh",display: "flex", justifyContent: "space-between" }}
-          >
-            <div>
-              <div className="image-container" style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-                <img
-                  style={{ borderRadius: "50%" }}
-                  src={billy}
-                  alt="profile"
-                /> <Typography style={{ paddingLeft: "5px" }} variant="subtitle1"><strong>Billy the Cat</strong></Typography>
+            <div
+              className="profile"
+              style={{ paddingTop: "1vh", display: "flex", justifyContent: "space-between" }}
+            >
+              <div>
+                <div className="image-container" style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+                  <img
+                    style={{ borderRadius: "50%" }}
+                    src={billy}
+                    alt="profile"
+                  /> <Typography style={{ paddingLeft: "5px" }} variant="subtitle1"><strong>Billy the Cat</strong></Typography>
+                </div>
+              </div>
+
+
+
+              <div>
+                <List
+                  component="nav"
+                  aria-label="Device settings"
+                  sx={{ bgcolor: "background.paper" }}
+                >
+                  <ListItem
+                    button
+                    id="lock-button"
+                    aria-haspopup="listbox"
+                    aria-controls="lock-menu"
+                    aria-label="when device is locked"
+                    aria-expanded={open ? "true" : undefined}
+                    onClick={handleClickListItem}
+                  >
+                    <ListItemText primary={options[selectedIndex]} />
+                  </ListItem>
+                </List>
+                <Menu
+                  id="lock-menu"
+                  anchorEl={anchorEl}
+                  open={privacyOpen}
+                  onClose={handlePrivacyClose}
+                  MenuListProps={{
+                    "aria-labelledby": "lock-button",
+                    role: "listbox",
+                  }}
+                >
+                  {options.map((option, index) => (
+                    <MenuItem
+                      key={option}
+                      disabled={index === 0}
+                      selected={index === selectedIndex}
+                      onClick={(event) => handleMenuItemClick(event, index)}
+                    >
+                      {option}
+                    </MenuItem>
+                  ))}
+                </Menu>
               </div>
             </div>
 
-
-
-            <div>
-              <List
-                component="nav"
-                aria-label="Device settings"
-                sx={{ bgcolor: "background.paper" }}
-              >
-                <ListItem
-                  button
-                  id="lock-button"
-                  aria-haspopup="listbox"
-                  aria-controls="lock-menu"
-                  aria-label="when device is locked"
-                  aria-expanded={open ? "true" : undefined}
-                  onClick={handleClickListItem}
-                >
-                  <ListItemText primary={options[selectedIndex]} />
-                </ListItem>
-              </List>
-              <Menu
-                id="lock-menu"
-                anchorEl={anchorEl}
-                open={privacyOpen}
-                onClose={handlePrivacyClose}
-                MenuListProps={{
-                  "aria-labelledby": "lock-button",
-                  role: "listbox",
-                }}
-              >
-                {options.map((option, index) => (
-                  <MenuItem
-                    key={option}
-                    disabled={index === 0}
-                    selected={index === selectedIndex}
-                    onClick={(event) => handleMenuItemClick(event, index)}
-                  >
-                    {option}
-                  </MenuItem>
-                ))}
-              </Menu>
-            </div>
-          </div>
-
-          <TextField
-            fullWidth
-            placeholder="What's happening?"
-            inputProps={{
-              maxlength: CHARACTER_LIMIT,
-            }}
-            value={values.name}
-            helperText={`${values.name.length}/${CHARACTER_LIMIT}`}
-            onChange={handleChange("name")}
-            margin="normal"
-            variant="filled"
-            rows={5}
-            multiline
-          />
+            <TextField
+              fullWidth
+              placeholder="What's happening?"
+              inputProps={{
+                maxlength: CHARACTER_LIMIT,
+              }}
+              value={values.name}
+              helperText={`${values.name.length}/${CHARACTER_LIMIT}`}
+              onChange={handleChange("name")}
+              margin="normal"
+              variant="filled"
+              rows={5}
+              multiline
+            />
 
 
 
-          {selectedImage && (
-            <div style={styles.preview}>
-              <img
-                src={URL.createObjectURL(selectedImage)}
-                style={styles.image}
-                alt="Thumb"
-              />
-              <Button color="error" style={{marginTop:"3px"}} startIcon={<CancelIcon/>}variant="outlined" onClick={removeSelectedImage}>
-                Remove Image
-              </Button>
-            </div>
-          )}
-
-          <hr />
-          <div
-            className="bottomBar"
-            style={{
-              paddingTop: "2vh",
-              display: "flex",
-              justifyContent: "space-between",
-            }}
-          >
-            <div style={{ display: "flex", justifyContent: "space-between" }}>
-              <Button variant="text" component="label">
-                <input
-                  accept="image/*"
-                  type="file"
-                  onChange={imageChange}
-                  hidden
+            {selectedImage && (
+              <div style={styles.preview}>
+                <img
+                  src={URL.createObjectURL(selectedImage)}
+                  style={styles.image}
+                  alt="Thumb"
                 />
-                <ImageRoundedIcon />
-              </Button>
-              <Button variant="text" component="label">
-                <EmojiEmotionsRoundedIcon />
+                <Button color="error" style={{ marginTop: "3px" }} startIcon={<CancelIcon />} variant="outlined" onClick={removeSelectedImage}>
+                  Remove Image
+                </Button>
+              </div>
+            )}
+
+            <hr />
+            <div
+              className="bottomBar"
+              style={{
+                paddingTop: "2vh",
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <Button variant="text" component="label">
+                  <input
+                    accept="image/*"
+                    type="file"
+                    onChange={imageChange}
+                    hidden
+                  />
+                  <ImageRoundedIcon />
+                </Button>
+                <Button variant="text" component="label">
+                  <EmojiEmotionsRoundedIcon />
+                </Button>
+              </div>
+              <Button
+                size="large"
+                endIcon={<SendIcon />}
+                variant="contained"
+              >
+                Send Post{" "}
               </Button>
             </div>
-            <Button
-              size="large"
-              endIcon={<SendIcon />}
-              variant="contained"
-            >
-              Send Post{" "}
-            </Button>
-          </div>
-        </Box>
+          </Box>
+        </Fade>
       </Modal>
     </div>
   );
